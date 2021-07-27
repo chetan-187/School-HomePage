5397300b5dd8d5c8894be9bb9d34345184cb6d09
Chetan L <lohkarec@gmail.com>
2021-07-27 22:58:18 +0530

    first commit

---
+++ P1/index.js
@@ -0,0 +1,17 @@
+let a;
+let time;
+let date;
+const options ={weekday:'long', year:'numeric', month:'long', day:'numeric'};
+
+
+setInterval(() => {
+
+a = new Date();
+date = a.toLocaleDateString( undefined , options);
+time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+document.getElementById('time').innerHTML = time + "<br> On " + date;
+
+
+
+
+}, 1000);
